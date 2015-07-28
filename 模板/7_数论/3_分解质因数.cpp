ce30bd480c2749f4366197e6162d39e9256117cb
fz <568573448@qq.com>
2015-07-28 17:35:39 +0800

    optimize

--- 模板/7_数论/3_分解质因数.cpp
+++ 模板/7_数论/3_分解质因数.cpp
@@ -1,14 +1,12 @@
 int a[1000000]= {},pcnt=0;
 void pdec(int n)
 {
-    int x=sqrt(n);
-    for (int i=1; prime[i]<=x; i++)
+    for (int i=1; prime[i]*prime[i]<=n; i++)
         if (n%prime[i]==0)
         {
             a[++pcnt]=prime[i];
             n/=prime[i];
             i--;
         }
-    if (n!=1)
-        a[++pcnt]=n;
+    if (n!=1) a[++pcnt]=n;
 }
